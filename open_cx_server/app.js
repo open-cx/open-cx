34a1d066873dec1c25f411c0b92b0a395436d722
Mark Meehan <mtimothy38@gmail.com>
2019-11-21 16:37:54 +0000

    added defaults for PORT, HOST and DB_CONNECTION

--- open_cx_server/app.js
+++ open_cx_server/app.js
@@ -8,22 +8,21 @@ const OPTS = { useUnifiedTopology: true, useNewUrlParser: true };
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 5000;
-const HOST = '0.0.0.0';
-
+const PORT = (process.env.PORT || 5000);
+const HOST = (process.env.HOST || "localhost");
+const DB_CONNECTION = (process.env.DB_CONNECTION || "mongodb://mongo:27017/testdb");
 const routes = require('./routes');
 
 // Middlewares
 app.use('/admin', require('./admin'))
 app.use(bodyParser.json());
 
-
 app.use('/', routes);
 app.use(cors());
 
 // Connects app to a MongoDB database
-mongoose.connect(process.env.DB_CONNECTION, OPTS, () => { console.log("Attempting connection to database") });
+mongoose.connect(DB_CONNECTION, OPTS, () => { console.log("Attempting connection to database") });
 
-app.listen(process.env.PORT || 5000);
+app.listen(PORT);
 
 console.log(`Running on http://${HOST}:${PORT}`);
