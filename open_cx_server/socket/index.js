612462bc1f0bd9eadad64dcf84a826257baf2218
Andre Rocha <andrefmrocha@live.com.pt>
2019-11-25 18:21:06 +0000

    Now emitting unread friend requests

--- open_cx_server/socket/index.js
+++ open_cx_server/socket/index.js
@@ -1,18 +1,35 @@
 const NodeCache = require('node-cache');
 const cache = new NodeCache();
+const FriendRequest = require('../models/friend_request');
+const User = require('../models/user');
 
 
 module.exports = (io) => {
-    io.on('connection',(socket) => {
-        console.log('User connected');
+    io.on('connection',async (socket) => {
+        console.info('User connected');
         const user = socket.handshake.headers['user_id'];
         if(user){
             cache.set(user, socket.id);
             socket.emit('success');
-    
+
+            const friendRequests = await FriendRequest.find( { receiver: user, unread: true } );
+
+            friendRequests.forEach(async friendRequest => {
+                const user = await User.findById(friendRequest.sender);
+                socket.emit('friend_request', user.fullname);
+            });
+
+            friendRequests.forEach(async (friendRequest) => {
+                friendRequest.unread = false;
+                await FriendRequest.findByIdAndUpdate(friendRequest.id, friendRequest);
+            });
+
+
             socket.on('disconnect', () => {
                 cache.del(user);
             });
+
+
         } else{
             socket.emit('failure',  "No user ID found");
         }
