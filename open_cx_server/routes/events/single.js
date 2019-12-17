6aaa5c492f0d9121ee383279d0311341344d0ec4
Marantesss <up201706917@fe.up.pt>
2019-12-17 10:51:35 +0000

    Added singlre request to event

--- open_cx_server/routes/events/single.js
+++ open_cx_server/routes/events/single.js
@@ -1,7 +1,16 @@
+let Event = require('../../models/event');
+
+
 const singleEvent = require('express').Router();
 
 singleEvent.get('/:postId', async (req, res) => {
-  res.send("Get request on /events/id");
+  try {
+    const event = await Event.findById(req.params.postId);
+    res.json(event);
+  }
+  catch (err) {
+    res.json({ message: err });
+  }
 });
 
 singleEvent.post('/:postId', async (req, res) => {
