abbd7a18999dcb6fb297e458eb080fea51a68474
MOAAS <miguel.moas@gmail.com>
2019-12-17 22:25:42 +0000

    fixed build

--- open_cx/lib/main.dart
+++ open_cx/lib/main.dart
@@ -41,7 +41,7 @@ class HomePage extends StatelessWidget {
                       style: TextStyle(fontSize: 32, color: Colors.white))),
               MyButton(x: 14, y: 60, title: "Ex", onPressed: () {}),
               MyButton(x: 40, y: 30, title: "Here", onPressed: () => Navigator.of(context).pushNamed("/agendAppMain")),
-              MyButton(x: 70, y: 60, title: "AgendApp", onPressed: () => Navigator.of(context).pushNamed("/agendAppMain"))
+              MyButton(x: 70, y: 60, title: "AgendAppskkit", onPressed: () => Navigator.of(context).pushNamed("/agendAppMain"))
             ],
           ),
         ),
