4b83f1dfadd81b9a028efa497f694cc0147c1181
Nuno <up201706162@fe.up.pt>
2019-12-20 12:57:19 +0000

    Reverted previous changes

--- open_cx_server/routes/rooms/index.js
+++ open_cx_server/routes/rooms/index.js
@@ -1,6 +1,10 @@
 const rooms = require('express').Router();
 
+const all = require('./all');
 const single = require('./single');
+
+rooms.get('/', all);
+rooms.post('/', all);
 
 rooms.get('/:postId', single);
 rooms.delete('/:postId', single);
