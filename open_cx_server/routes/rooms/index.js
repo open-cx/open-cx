359119484ff1a0a74a295aadf0f036cdc44638ed
Nuno <up201706162@fe.up.pt>
2019-12-20 12:49:27 +0000

    Deleted all

--- open_cx_server/routes/rooms/index.js
+++ open_cx_server/routes/rooms/index.js
@@ -1,10 +1,6 @@
 const rooms = require('express').Router();
 
-const all = require('./all');
 const single = require('./single');
-
-rooms.get('/', all);
-rooms.post('/', all);
 
 rooms.get('/:postId', single);
 rooms.delete('/:postId', single);
